619fed7c3b4d339ffba599a3a98050142eac0d2a
Rahul Vij <rahulv@discovery.co.za>
2018-08-31 11:08:35 +0200

    spring config related changes. Changing application.properties to boostrap.properties for spring cloud config. Note - bootstrap.properties loads before the application.properties

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -8,6 +8,7 @@
 package com.in28minutes.microservices.limitsservice;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public LimitsConfiguration retrieveLimitsFromConfigurations() {
-        return new LimitsConfiguration(1000, 1);
+        return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 }
